ce75bcfc48db21d83ec14f2a755c07d462ae8add
eric arieta <126834122+earieta21@users.noreply.github.com>
2023-05-09 17:58:15 -0700

    Add files via upload

---
+++ block18.js
@@ -0,0 +1,18 @@
+//unit test
+// expect a non numeric argument 
+//expect a null argument 
+//expect a non array argument 
+//expect a empty array 
+
+
+
+//Functional Test
+//expect to informe if the cart is empty
+//expect to show their order summary
+//expect to show the shipping information
+//expect to show the payment information
+//expect to show the purchase confirmation order
+//expect the recovery of lost pasword
+//expect the information required for creating a accout 
+//expect the track of the order 
+//expect an invalid shipping address 
